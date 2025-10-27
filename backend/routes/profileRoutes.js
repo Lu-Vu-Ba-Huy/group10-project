239a9cce3249565062447d58004eb6c964119471
Diem Nguyen <ADMIN@jcognmeeid.com>
2025-10-27 13:42:28 +0700

    fix

--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
-const User = require('../models/User');
+const User = require('../models/user');
 const authMiddleware = require('../middleware/auth');
 
 // Cấu hình multer để upload file
